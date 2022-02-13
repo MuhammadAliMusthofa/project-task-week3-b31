db56d53d94018fa02adc29276fdfecab0ccdde4f
Muhammad Ali Musthofa <muhammadalimusthofa786@gmail.com>
2022-02-13 20:34:54 +0700

    firstpush

--- index.js
+++ index.js
@@ -10,38 +10,12 @@ app.set('view engine', 'hbs');
 app.use('/public', express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }));
 
-// true => sudah login
-// false => belum login
 const isLogin = true;
-
-//Membuat Array Object yg akan menyimpan Data blog list
-// const blogs = [
-//   {
-//     projectName: 'Judul',
-//     startDate: 'Judul',
-//     endDate: 'Judul',
-//     description: 'Judul',
-//   },
-// ];
-//bulan
-// let month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'Desember'];
 
 // Set endpoint
 app.get('/', function (req, res) {
   res.send('Hello World');
 });
-
-// app.get('/home', function (req, res) {
-//   res.send(`<script> alert('Ini adalah halaman home')</script>`);
-// });
-// app.get('/add-project', function (req, res) {
-//   res.send(`<script> alert('Tambahin Project yuk !!')</script>`);
-//   // console.log('kamu sedand berada di hLmN add');
-// });
-// app.get('/contact', function (req, res) {
-//   // res.send(`<script> alert('MAu colab?? contact aja aku isi di bawah!!!')</script>`);
-//   console.log('kamu sedand berada di hLmN contact');
-// });
 
 app.get('/home', function (req, res) {
   res.render('home');
@@ -60,11 +34,6 @@ app.get('/home', function (req, res) {
 
   res.render('home', { isLogin: isLogin, blogs: dataBlogs });
 });
-
-// //add
-// app.get('/add-blog', function (req, res) {
-//   res.render('form-blog');
-// });
 
 //DELETE list blog
 app.get('/delete-project/:index', function (req, res) {
@@ -90,16 +59,6 @@ app.post('/add-project', function (req, res) {
             Tanggal Mulai: ${start},
             Berakhir Pada: ${end},
             Descripsi: ${description}`);
-  // let blog = {
-  //   project: project,
-  //   start,
-  //   end,
-  //   description,
-  // };
-
-  // blogs.push(blog);
-  // res.redirect('/home');
-  // res.redirect('/add-project');
 });
 
 app.get('/home/:id', function (req, res) {
@@ -118,14 +77,3 @@ const port = 5000;
 app.listen(port, function () {
   console.log(`server running on port ${port}`);
 });
-
-// function getFullTime(time) {
-//   let date = time.getDate();
-//   let monthIndex = time.getMonth();
-//   let year = time.getFullYear();
-
-//   let hours = time.getHours();
-//   let minutes = time.getMinutes();
-
-//   return ` ${date} ${month[monthIndex]}  ${year} ${hours}:${minutes} WIB`;
-// }
